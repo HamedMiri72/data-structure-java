38ea069a5060a8d538d75a43ffd881f7d5c5f932
HamedMiri72 <hamedmiri72@gmail.com>
2025-02-06 14:43:25 +0000

    Filling an Array with Default Values

---
+++ src/main/java/com/hamed/FiilingArrayDefault.java
@@ -0,0 +1,16 @@
+package com.hamed;
+
+import java.util.Arrays;
+
+public class FiilingArrayDefault {
+
+    public static void main(String[] args) {
+
+
+        int[] numbers = new int[9];
+
+        Arrays.fill(numbers, 7);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
